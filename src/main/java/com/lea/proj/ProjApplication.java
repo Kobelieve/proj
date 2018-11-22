e32f7956d0c8e8e3a3a34f5b447414c060e9055e
Tao <15622719535@163.com>
2018-11-22 17:47:55 +0800

    主干啦

--- src/main/java/com/lea/proj/ProjApplication.java
+++ src/main/java/com/lea/proj/ProjApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 启动器
+ * 启动器，这里是master
  */
 @SpringBootApplication
 @MapperScan("com.lea.proj.dao")
