7c30a067090d4445b8a942a65c675ec31615ecab
Tao <15622719535@163.com>
2018-12-24 09:37:57 +0800

    second commit!

--- src/main/java/com/lea/proj/ProjApplication.java
+++ src/main/java/com/lea/proj/ProjApplication.java
@@ -4,23 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 启动器，这里是dev
- */
 @SpringBootApplication
 @MapperScan("com.lea.proj.dao")
 public class ProjApplication {
-
-    public void master() {
-        
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjApplication.class, args);
     }
     
-    public void dev() {
-        System.out.println("分支1");
-        System.out.println("分支2");
-    }
+
 }
