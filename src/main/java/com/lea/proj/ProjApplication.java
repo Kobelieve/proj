16d5faf27b0af289c87987d7ac1fb243bd97eb0f
Tao <15622719535@163.com>
2019-08-29 09:35:34 +0800

    test1
    test2

--- src/main/java/com/lea/proj/ProjApplication.java
+++ src/main/java/com/lea/proj/ProjApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1
+ */
 @SpringBootApplication
 @MapperScan("com.lea.proj.dao")
 public class ProjApplication {
